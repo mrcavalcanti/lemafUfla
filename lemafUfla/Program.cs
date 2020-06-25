b262d2678f2a05779e2de3076cc9ab943ab181ff
Marcelo Rezende Cavalcanti <marcelo@swg2.com.br>
2020-06-25 16:14:45 -0300

    Recursive Main

--- lemafUfla/Program.cs
+++ lemafUfla/Program.cs
@@ -7,23 +7,18 @@ namespace lemafUfla
     {
         static async Task Main(string[] args)
         {
+            await EnterWord();
+        }
+
+        public static async Task EnterWord()
+        {
             string word = ReadWord.Read();
             Console.WriteLine();
 
             var itemDictionary = await Dictionary.ProcessDictionary(word);
             Console.WriteLine(itemDictionary.Index + " - " + itemDictionary.Word);
             Console.WriteLine();
-
-            /*
-            var repositories = await Dictionary.ProcessDictionary(word);
-
-            foreach (var repo in repositories)
-            {
-                Console.WriteLine(repo.Value1);
-                Console.WriteLine(repo.Value2);
-                Console.WriteLine();
-            }
-            */
+            await EnterWord();
         }
     }
 }
